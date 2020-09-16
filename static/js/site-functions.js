619eabda6efdebb3b4633363f8acddcf06dcbc2d
Steven Tammen <steven@steventammen.com>
2020-09-16 16:05:01 -0400

    Add functions for cookies

--- static/js/site-functions.js
+++ static/js/site-functions.js
@@ -121,6 +121,29 @@ function cleanupMobileInlineStyles(trigger) {
   }
 }
 
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 var trigger = window.matchMedia("(min-width: 48em)")
 // Call listener function at run time
 cleanupMobileInlineStyles(trigger)
